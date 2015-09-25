6588ce58c8b49946906845bfb8eeebcfd4d00b62
dkidney <darrenkidney@yahoo.co.uk>
2015-09-25 10:20:00 +0100

    update to gui manual

--- inst/doc/gibbonsecr_gui.R
+++ inst/doc/gibbonsecr_gui.R
@@ -1,1 +1,15 @@
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages(c("CircStats", "fields", "MASS", "nlme", "secr", "tcltk2"))
+
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages("https://github.com/dkidney/gibbonsecr/raw/master/binaries/gibbonsecr_1.0.zip",
+#                   repos = NULL, type = "win.binary")
+
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages("https://github.com/dkidney/gibbonsecr/raw/master/binaries/gibbonsecr_1.0.tgz",
+#                   repos = NULL, type = "mac.binary")
+
+## ----eval=FALSE----------------------------------------------------------
+#  library(gibbonsecr)
+#  gibbonsecr_gui()
 
